18ae7a15c34b0c2e1ff96cc821150e8a8f156c3a
unknown <wuzeji>
2020-12-08 16:31:41 +0800

    a

--- lodash/wuzejimaya-lodash.js
+++ lodash/wuzejimaya-lodash.js
@@ -1,7 +1,7 @@
 var wuzejimaya = function () {
   function chunk(array, size = 1) {
     let res = []
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length;) {
       let tem = []
       for (let j = 0; j < size && i < array.length; j++) {
         tem.push(array[i++])
@@ -10,7 +10,21 @@ var wuzejimaya = function () {
     }
     return res
   }
+  function compact(array) {
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] !== false && array[i] !== null && array[i] !== 0 &&
+        array[i] !== "" && array[i] !== undefined && array[i] === array[i]) {
+        res.push(array[i])
+      }
+    }
+    return res
+  }
+  function drop(array, n = 1) {
+
+  }
   return {
     chunk,
+    compact,
   }
 }()
