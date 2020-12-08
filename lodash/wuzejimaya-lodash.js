56362f8f3ee0c03e4b64eb801bcd5b89632a5c5a
unknown <wuzeji>
2020-12-08 15:28:22 +0800

    a

--- lodash/wuzejimaya-lodash.js
+++ lodash/wuzejimaya-lodash.js
@@ -1,3 +1,12 @@
 var wuzejimaya = function () {
-  
+  function concat(array, values) {
+    let res = ''
+    for (let i = 0; i < array.length; i++) {
+      res += array[i]
+    }
+    return res
+  }
+  return {
+    concat,
+  }
 }()
